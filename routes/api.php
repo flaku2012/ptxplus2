e7d556df2775809050ac1963a715501d900b6bec
flaku2012 <72572975+flaku2012@users.noreply.github.com>
2021-03-24 21:29:53 +0100

    1.0.6

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::Apiresource('/categories', CategoriesController::class);
+Route::apiResource('/categories', CategoriesController::class);
